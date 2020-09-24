e3ef9f509d73eb47e7d84a01e306222bcce46416
MarcosRC1998 <mrcmarcos100@gmail.com>
2020-09-24 12:54:09 -0300

    Add error handling

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -6,6 +8,20 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Ok' });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res
+      .status(err.statusCode)
+      .json({ status: 'error', error: err.message });
+  }
+
+  console.log(err);
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
 });
 
 app.listen(3333, () => {
